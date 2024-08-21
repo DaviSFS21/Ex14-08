13055db863facf4a75fbcd9311b651cd02d51aea
Davi Soares <davisfs2110@gmail.com>
2024-08-21 20:03:23 -0300

    feat: Adiciona a resolução do décimo segundo exercício

---
+++ src/MaiorNumero.java
@@ -0,0 +1,9 @@
+public class MaiorNumero {
+    public static void main(String[] args) {
+        int x=10;
+        int y=25;
+
+        if(x > y) { System.out.println(x); }
+        else { System.out.println(y); }
+    }
+}
