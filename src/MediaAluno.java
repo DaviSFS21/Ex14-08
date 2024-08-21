5aba3fea78675840604e7467087f23c9e98913d0
Davi Soares <davisfs2110@gmail.com>
2024-08-20 22:15:56 -0300

    feat: Adiciona a resolução do décimo exercício

---
+++ src/MediaAluno.java
@@ -0,0 +1,22 @@
+public class MediaAluno {
+    public Double nota1, nota2;
+
+    public MediaAluno(Double nota1, Double nota2) {
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    public String verifAprov(Double nota1, Double nota2) {
+        if (((nota1 + nota2) / 2) >= 6) {
+            return "Aprovado";
+        } else {
+            return "Reprovado";
+        }
+    }
+
+    public static void main(String[] args) {
+        MediaAluno mediaJoao = new MediaAluno(5.0, 7.0);
+
+        System.out.printf("Nota 1: %f%nNota 2: %f%n%nSituação: %s", mediaJoao.nota1, mediaJoao.nota2, mediaJoao.verifAprov(5.0, 7.0));
+    }
+}
