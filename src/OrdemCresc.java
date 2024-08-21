99644232bc2e947e943cbe54199ee34c18f82fad
Davi Soares <davisfs2110@gmail.com>
2024-08-21 20:04:35 -0300

    feat: Adiciona a resolução do décimo terceiro exercício

---
+++ src/OrdemCresc.java
@@ -0,0 +1,9 @@
+public class OrdemCresc {
+    public static void main(String args[]) {
+        int x=100;
+        int y=25;
+
+        if(x > y) { System.out.println(x + " > " + y); }
+        else { System.out.println(y + " > " + x); }
+    }
+}
