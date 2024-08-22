d9a8f6b0fef050f6309076556c62e85e56f18a41
davisfs21 <davisfs2110@gmail.com>
2024-08-22 14:16:39 -0300

    feat: Adiciona o início da solução do décimo quinto exercício

---
+++ src/HoraExtra.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class HoraExtra {
+    int HrsTrab;
+    double SalHora;
+
+    public HoraExtra(int HrsTrab, double SalHora) {
+        this.HrsTrab = HrsTrab;
+        this.SalHora = SalHora;
+    }
+
+    public double calcSalFinal() {
+        return (SalHora * 160) + (Math.max(0,(HrsTrab - 160))*SalHora*1.5);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o salário por hora: ");
+        double SalHora = sc.nextDouble();
+        System.out.println("Digite a quantia de horas trabalhadas: ");
+        int HrsTrab = sc.nextInt();
+
+        HoraExtra gabriel = new HoraExtra(HrsTrab, SalHora);
+
+        System.out.printf("O salário total do funcionário é: R$%.2f",gabriel.calcSalFinal());
+
+        sc.close();
+    }
+}
