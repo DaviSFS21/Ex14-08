a7b9543e09eed43258af166a9cdb345a6cc70bf7
Davi Soares <davisfs2110@gmail.com>
2024-08-22 17:30:11 -0300

    fix: Corrige erros de saída e cálculo do código e inclui um teste de mesa ao texto

--- src/Idade.java
+++ src/Idade.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class Idade {
-    int dias,meses,anos;
+    int dias, meses, anos;
 
     public Idade(int dias, int meses, int anos) {
         this.dias = dias;
@@ -7,19 +9,20 @@ public class Idade {
         this.anos = anos;
     }
 
-    public Idade(int dias, int meses) {
-        this.dias = dias;
-        this.meses = meses;
-        this.anos = 0;
-    }
-
     public int idadeEmDias() {
         return(dias + meses*30 + anos*365);
     }
 
     public static void main(String[] args) {
-        Idade pessoa = new Idade(23,9,18);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seus anos de vida: ");
+        int anos = sc.nextInt();
+        System.out.println("Digite seus meses de vida: ");
+        int meses = sc.nextInt();
+        System.out.println("Digite seus dias de vida: ");
+        int dias = sc.nextInt();
+        Idade idade = new Idade(dias, meses, anos);
 
-        System.out.printf("A sua idade em dias é de: %d",pessoa.idadeEmDias());
+        System.out.printf("A sua idade em dias é de: %d",idade.idadeEmDias());
     }
 }
