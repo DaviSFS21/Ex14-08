0ebb5d59adf9a88ecab6fdbfb76f734b1ec19426
Davi Soares <davisfs2110@gmail.com>
2024-08-21 22:29:22 -0300

    fix: Corrige erros de saída e cálculo do código e inclui um teste de mesa ao texto

--- src/GastosTrim.java
+++ src/GastosTrim.java
@@ -7,13 +7,24 @@ public class GastosTrim {
         this.mar = mar;
     }
 
-    public Double gastosTotais(Double jan, Double fev,Double mar) {
+    public Double somarGastos(Double jan, Double fev, Double mar) {
         return jan + fev + mar;
     }
 
     public static void main(String[] args) {
-        GastosTrim Cube = new GastosTrim(15000.0, 23000.0, 17000.0);
+        GastosTrim Cube = new GastosTrim(20000.0, 23000.0, 17000.0);
 
-        System.out.printf("Nota 1: %f%nNota 2: %f%n%nSituação: %s", Cube.jan, Cube.fev, Cube.gastosTotais(Cube.jan, Cube.fev, Cube.mar));
+        System.out.printf("Gastos em janeiro: R$%.2f%nGastos em fevereiro: R$%.2f%nGastos em março: R$%.2f%n%nGastos no trimestre: R$%.2f%nMédia de gastos: R$%.2f", Cube.jan, Cube.fev, Cube.mar, Cube.somarGastos(Cube.jan, Cube.fev, Cube.mar), (Cube.somarGastos(Cube.jan, Cube.fev, Cube.mar)/3));
     }
 }
+
+/*
+| Linha | Ação                                            | `Cube.jan` | `Cube.fev` | `Cube.mar` | somarGastos() | somarGastos/3 | Saída                                                                                                    |
+|-------|-------------------------------------------------|------------|------------|------------|---------------|---------------|---------------------------------------------------------------------------------------------------------|
+| 10    | Criação do objeto `Cube`                        | `20000.0`  | `23000.0`  | `17000.0`  | -             | -             | Gastos em janeiro: R$20000.00                                                                           |
+| 13    | Impressão dos valores de `jan`, `fev`, e `mar`  | `20000.0`  | `23000.0`  | `17000.0`  | -             | -             | Gastos em fevereiro: R$23000.00                                                                         |
+| 13    | Chamada de `somarGastos` e cálculo da soma      | `20000.0`  | `23000.0`  | `17000.0`  | `60000.0`     | -             | Gastos em março: R$17000.00                                                                             |
+| 13    | Impressão da soma dos gastos no trimestre       | `20000.0`  | `23000.0`  | `17000.0`  | `60000.0`     | -             |                                                                                                         |
+| 13    | Cálculo da média dos gastos                     | `20000.0`  | `23000.0`  | `17000.0`  | `60000.0`     | `20000.0`     | Gastos no trimestre: R$60000.00                                                                         |
+| 13    | Impressão da média dos gastos                   | `20000.0`  | `23000.0`  | `17000.0`  | `60000.0`     | `20000.0`     | Média de gastos: R$20000.00                                                                             |
+*/
