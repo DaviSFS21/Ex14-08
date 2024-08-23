bd04baeaca001e70f120e0e4fbe3b95bbd32d07d
Davi Soares <davisfs2110@gmail.com>
2024-08-22 22:41:12 -0300

    feat: Adiciona os testes de mesa dos dois últimos exercícios

--- src/TempoPartida.java
+++ src/TempoPartida.java
@@ -8,7 +8,11 @@ public class TempoPartida {
 
     public int calcularTempo() {
         if(fim > inicio) {
-            return fim - inicio;
+            if(fim == inicio) {
+                return 1;
+            } else {
+                return fim - inicio;
+            }
         } else {
             int i = (24 - inicio) + fim;
             return Math.min(i, 24);
@@ -16,8 +20,16 @@ public class TempoPartida {
     }
 
     public static void main(String[] args) {
-        TempoPartida partida = new TempoPartida(14, 14);
+        TempoPartida partida = new TempoPartida(10, 14);
 
         System.out.println("Tempo da partida: " + partida.calcularTempo());
     }
 }
+
+/*
+| Linha | Ação                                  | inicio | fim | calcularTempo | Saída                     |
+|-------|---------------------------------------|--------|-----|---------------|---------------------------|
+|   4   | Criação do construtor `TempoPartida`  |  -     |  -  |       -       |                           |
+|   23  | Criação do objeto `TempoPartida`      |  10    | 14  |       -       |                           |
+|   25  | Saída do cálculo                      |  10    | 14  |       4       | Tempo da partida: 4       |
+*/
